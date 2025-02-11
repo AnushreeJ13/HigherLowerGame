33ffe81123a32772fcad2388ed475767648ce811
AnushreeJ13 <Jainanushree2005@gmail.com>
2025-02-12 03:00:54 +0530

    improved result

--- src/EndScreen.js
+++ src/EndScreen.js
@@ -1,13 +1,18 @@
 // EndScreen.js
 import React from "react";
-
+import end from './end.gif';
 function EndScreen({ score, highScore, restart, returnToMenu }) {
   return (
     <div className="end-container">
       <h2>Game Over!</h2>
+      
+      <div className="end-container1">
       <p>Final Score: {score}</p>
       <p>High Score: {highScore}</p>
+      </div>
+      
       <div className="end-buttons">
+        <img src={end}></img>
         <button onClick={restart}>Try Again</button>
         <button onClick={returnToMenu}>Main Menu</button>
       </div>
